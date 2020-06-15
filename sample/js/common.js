2d225db0b777c575151f522451b307666e894f1c
cokka-ram <148430yuno@gmail.com>
2020-06-15 21:02:57 +0900

    bxsliderの書き方

--- sample/js/common.js
+++ sample/js/common.js
@@ -1,9 +1,22 @@
 $(function(){
   'use strict';
   
-  $('#pageTop').on('click',function(){
+  let pageTop = function( time ){
     $('html,body').animate({
       scrollTop: 0
-    });
+    },time);  
+  }
+  
+  $('#pageTop').on('click',function(){
+      pageTop(600);
+  });
+  
+  $('#slideshow').bxSlider({
+    auto: true,
+    controls: false,
+    mode: 'fade',
+    pager: false,
+    spead: 1000,
+    touchEnabled: false,
   });
 });
